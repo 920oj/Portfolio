a384ff125e555caa03ca673ba9068ba04397f7b9
Junya Oiwa <0920oiwa@gmail.com>
2019-09-20 21:55:24 +0900

    Articles仮実装

--- pages/articles.vue
+++ pages/articles.vue
@@ -21,18 +21,37 @@ export default {
   data() {
     return {
       hatenaArticles: null,
+      qiitaArticles: null,
     }
   },
   mounted() {
-    const self = this;
-    this.$axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2F920oj.hatenablog.com%2Frss')
+    this.getHatena();
+  },
+  methods: {
+    getHatena() {
+      const self = this;
+      this.$axios.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2F920oj.hatenablog.com%2Frss')
       .then(function(response){
         console.log(response.data.items);
         self.hatenaArticles = response.data.items;
+        self.getQiita();
+      })
+      .catch(function(error){
+        console.log(error);
+        self.getQiita();
+      })
+    },
+    getQiita() {
+      const self = this;
+      this.$axios.get('https://qiita.com/api/v2/items?page=1&per_page=100&query=user%3A920oj')
+      .then(function(response){
+        console.log(response.data);
+        self.qiitaArticles = response.data;
       })
       .catch(function(error){
         console.log(error);
       })
+    }
   }
 }
 </script>
