41143526cbfb70824fc025913dca3931d531ac84
Junya Oiwa <0920oiwa@gmail.com>
2019-09-28 12:57:36 +0900

    OGP等記載追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,16 +6,27 @@ export default {
   */
   head: {
     // title: process.env.npm_package_name || '',
-    title: `おーじぇいWEB`,
+    title: `920oj.net - OJのポートフォリオ`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
       { 
         hid: 'description', 
         name: 'description', 
-        content: 'おーじぇいことJunya OiwaのポートフォリオWebサイトです。自己紹介や過去に作ったものなどの情報を掲載しています。' 
+        content: 'おーじぇいこと大岩潤矢のポートフォリオWebサイトです。自己紹介や過去に作ったものなどの情報を掲載しています。' 
       },
-      { property: 'og:url', content: 'https://920oj.net/'}
+      { hid: 'description', name: 'description', content: 'OJこと大岩潤矢のポートフォリオです。自己紹介や過去に作ったものなどの情報を掲載しています。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: '920oj.net - OJのポートフォリオ' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://920oj.net/' },
+      { hid: 'og:title', property: 'og:title', content: '920oj.net - OJのポートフォリオ' },
+      { hid: 'og:description', property: 'og:description', content: 'OJこと大岩潤矢のポートフォリオです。自己紹介や過去に作ったものなどの情報を掲載しています。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://920oj.net/ogp.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@920oj' },
+      { name: 'twitter:title', content: '920oj.net - OJのポートフォリオ'},
+      { name: 'twitter:description', content: 'OJこと大岩潤矢のポートフォリオです。自己紹介や過去に作ったものなどの情報を掲載しています。'}
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
